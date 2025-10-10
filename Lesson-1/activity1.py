624ae5ab72f810addd89cca3f8cd71920abe9495
Pravasien <Pranav.skv3@gmail.com>
2025-10-10 20:09:54 +0800

    Add files via upload

---
+++ Lesson-1/activity1.py
@@ -0,0 +1,15 @@
+a=str(input("Enter your grade: "))
+print("your grade is: ",a)
+
+print("The sum of 16 and 24 is:",24+16)
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter your second number: "))
+if a>b:
+    print(a,"is greater than",b)
+elif a==b:
+    print(a,"is equal to",b)
+elif a<b:
+    print(a,"is less than",b)
+else:
+    print("Enter a valid input")
