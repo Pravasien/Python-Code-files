6efd6dcd5f9b2d3b4e284cfc284defc2341c8a07
Pravasien <Pranav.skv3@gmail.com>
2025-10-22 18:59:11 +0800

    Add files via upload

---
+++ Congratulation Message/index.py
@@ -0,0 +1,4 @@
+greeting = "Congratulations"
+User_Name=input("Enter your name: ")
+User_Compeitition=input("Enter the competition you participated in: ")
+print(greeting, User_Name, "! You have successfully participated in the ", User_Compeitition, " competition.")
