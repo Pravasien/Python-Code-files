485c4a459788f194d13c9e070158250a8f0779f2
Pravasien <Pranav.skv3@gmail.com>
2025-10-22 18:45:49 +0800

    Add files via upload

---
+++ Storing Birthdays/index.py
@@ -0,0 +1,4 @@
+day=int(input("Enter the day of your birthday (1-31): "))
+month=int(input("Enter the month of your birthday (1-12): "))
+year=int(input("Enter the year of your birthday (4 digits): "))
+print("Your birthday is on ",day,"/",month,"/",year)
