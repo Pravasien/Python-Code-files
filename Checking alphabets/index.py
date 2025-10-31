99e5d2782aa6ad2fba81c485c57a873685442408
Pravasien <Pranav.skv3@gmail.com>
2025-10-31 19:07:57 +0800

    Add files via upload

---
+++ Checking alphabets/index.py
@@ -0,0 +1,6 @@
+User_Input=input("Enter a character: ")
+if User_Input.isalpha():
+    print(User_Input," is an alphabet.")
+else:
+    print(User_Input," is not an alphabet.")
+
---
+++ Checking alphabets/index2.py
@@ -0,0 +1,5 @@
+User_Input=input("Enter a number: ")
+if User_Input.isdigit():
+    print(User_Input," is a number.")
+else:
+    print(User_Input," is not a number.")
