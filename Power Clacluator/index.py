8fdd87a9290a6c72d444c78f150f1a6e322324b0
Pravasien <Pranav.skv3@gmail.com>
2025-11-07 18:32:47 +0800

    Add files via upload

---
+++ Power Clacluator/index.py
@@ -0,0 +1,4 @@
+Base=float(input("Enter the Base: "))
+User_Exponent=float(input("Enter the User Exponent: "))
+Result=Base**User_Exponent
+print("The Result of ",Base,"to the power of",User_Exponent,"is:",Result)
