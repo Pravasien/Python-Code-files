5bbc9fb1c58297abdc91473211803f0d8a07dae2
Pravasien <Pranav.skv3@gmail.com>
2025-11-19 20:40:21 +0800

    Add files via upload

---
+++ Create square using turtle/index.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().setup(800,600)
+turtle.Screen().bgcolor("lightblue")
+Square= turtle.Turtle()
+Square.forward(100)
+Square.right(90)
+
+Square.forward(100)
+Square.right(90)
+
+Square.forward(100)
+Square.right(90)
+
+Square.forward(100)
+Square.right(90)
+
+turtle.done()
