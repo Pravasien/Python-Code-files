3d3f531eba4c8cf7e8bd83bf859221d18ded4320
Pravasien <Pranav.skv3@gmail.com>
2026-01-07 18:43:49 +0800

    Add files via upload

--- index2.py
+++ index2.py
@@ -1,24 +1,12 @@
-def hotel_cost(nights):
-    return 140*nights
-def plane_ride_cost(city):
-    if city=="Banglore":
-        return 750
-    elif city=="Chennai":
-        return 300
-    elif city=="Hyderabad":
-        return 450
-    elif city=="Dheli":
-        return 650
-def rental_car_cost(days):
-    if days>=7:
-        return days*40-30
-    elif days>=3:
-        return days*40-15
-    else:
-        return days*40
-def trip_cost(city,days,spending_money):
-    return rental_car_cost(days)+hotel_cost(days)+plane_ride_cost(city)+spending_money
-print("cost of car rental for 5 days:",rental_car_cost(5))
-print("cost of plane ride to Banglore:",plane_ride_cost("Banglore"))
-print("cost of hotel for 5 nights:",hotel_cost(5))
-print(trip_cost("Banglore",5,600))
+import random
+import time 
+def getRandomDate(startDate,endDate):
+    print("Printing random date between",startDate,"and",endDate)
+    randomGenerator=random.random()
+    dateFormat='%m/%d/%Y'
+    startTime=time.mktime(time.strptime(startDate,dateFormat))
+    endTime=time.mktime(time.strptime(endDate,dateFormat))
+    randomTime=startTime + randomGenerator * (endTime - startTime)
+    randomDate=time.strftime(dateFormat,time.localtime(randomTime))
+    return randomDate
+print("Random Date=",getRandomDate("1/1/2025","12/31/2025"))
