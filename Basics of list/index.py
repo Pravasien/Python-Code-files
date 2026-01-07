ee5b85c54702eac82cfcccd6a939c7ee35fa09bb
Pravasien <Pranav.skv3@gmail.com>
2026-01-07 20:09:42 +0800

    Add files via upload

---
+++ Basics of list/index.py
@@ -0,0 +1,12 @@
+List_1=[]
+print(List_1)
+
+List_2=[1,2,3,4,5,6,7,8,9,10]
+print(List_2)
+
+list_3=["Football","Badminton","Cricket"]*5
+print(list_3)
+
+List_4=["Hello" ,"world!"]
+Reverse_List=List_4[::-1]
+print(Reverse_List)
