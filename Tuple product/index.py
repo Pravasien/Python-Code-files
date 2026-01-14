0da431cbeb7bb449d8f02c60eb71544b6d707954
Pravasien <Pranav.skv3@gmail.com>
2026-01-14 19:15:27 +0800

    Add files via upload

---
+++ Tuple product/index.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num4 = int(input("Enter number 4: "))
+num5 = int(input("Enter number 5: "))
+user_tuple = (num1, num2, num3, num4, num5)
+result = user_tuple[0] * user_tuple[1] * user_tuple[2] * user_tuple[3] * user_tuple[4]
+print(result)
