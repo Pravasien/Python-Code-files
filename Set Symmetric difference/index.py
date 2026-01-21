467cf204eccccc6b4bb1b106ebbdb86678cb0abe
Pravasien <Pranav.skv3@gmail.com>
2026-01-21 19:20:11 +0800

    Add files via upload

---
+++ Set Symmetric difference/index.py
@@ -0,0 +1,8 @@
+set1 = {"blue", "green", "red", "yellow", "orange", "purple", "pink", "brown","white", "black", "gray", "cyan"}
+set2 = {"blue", "yellow", "cyan", "magenta", "lime", "gold", "silver", "black","purple", "red", "white","green"}
+
+print("The set difference in these two sets are ",set1.symmetric_difference(set2))
+
+set3={"1","2","3","4","5","6","7","8","9","10"}
+set4={"5","6","7","8","9","10","11","12","13"}
+print("The set difference in these two sets are ",set3.symmetric_difference(set4))
