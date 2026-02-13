32bbf5789f8047803c192444dbef8c74f911e246
Pravasien <Pranav.skv3@gmail.com>
2026-02-13 20:28:59 +0800

    Add files via upload

--- index2.py
+++ index2.py
@@ -1,12 +1,19 @@
-import random
-import time 
-def getRandomDate(startDate,endDate):
-    print("Printing random date between",startDate,"and",endDate)
-    randomGenerator=random.random()
-    dateFormat='%m/%d/%Y'
-    startTime=time.mktime(time.strptime(startDate,dateFormat))
-    endTime=time.mktime(time.strptime(endDate,dateFormat))
-    randomTime=startTime + randomGenerator * (endTime - startTime)
-    randomDate=time.strftime(dateFormat,time.localtime(randomTime))
-    return randomDate
-print("Random Date=",getRandomDate("1/1/2025","12/31/2025"))
+"""Create a python program to create a flashcard using object-oriented programming. A flashcard is a two-sided card with information on both sides that can assist memory. A question and an answer are usually printed on one side of a flashcard. Follow these steps to complete the activity - 1. From the user, take the input for a word and its definition. 2. To assign values for Word and Meaning, create a class called flashcard and use the __init__() function. 3. We'll use the __str__() function to get a string with the term and its definition. 4. Save the strings that have been returned in a list. 5. To print all of the stored flashcards, use a while loop."""
+class flashcard:
+	def __init__(self, word, meaning):
+		self.word = word
+		self.meaning = meaning
+	def __str__(self):
+		return self.word+' ( '+self.meaning+' )'	
+flash = []
+print("welcome to flashcard application")
+while(True):
+	word = input("enter the name you want to add to flashcard : ")
+	meaning = input("enter the meaning of the word : ")
+	flash.append(flashcard(word, meaning))
+	option = int(input("enter 0 , if you want to add another flashcard otherwise enter 1 : "))
+	if(option):
+		break
+print("\nYour flashcards")
+for i in flash:
+	print(">", i)
